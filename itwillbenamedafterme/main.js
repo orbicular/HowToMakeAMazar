a8284f9ec2bd590acae91168944bc8320c152495
orbicular <62100125+orbicular@users.noreply.github.com>
2020-11-16 16:03:23 +0800

    it will be named after me

---
+++ itwillbenamedafterme/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    animateDiv('#audio');
+});
+
+function makeNewPosition() {
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+}
+
+function animateDiv(myclass) {
+    var newq = makeNewPosition();
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 20000, function () {
+        animateDiv(myclass);
+    });
+
+};
+
+function play() {
+    var audio = document.getElementById("myaudio");
+    audio.currentTime = Math.floor(Math.random() * 90);
+    audio.play();
+    console.log(audio.currentTime);
+    setTimeout(function () {
+        audio.pause();
+    }, 10000);
+}
